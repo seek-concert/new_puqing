e4202ee756502685ae6b96ca40c4ec52037886bb
404status <fish.taixian@gmail.com>
2019-07-06 12:01:46 +0800

    添加富文本插件等文件、修改新闻分页

--- resources/views/pqadmin/news/news_detail.blade.php
+++ resources/views/pqadmin/news/news_detail.blade.php
@@ -71,13 +71,13 @@
                     <div class="line_grid">
                         <div class="g_3"><span class="label">作者<span class="must">*</span></span></div>
                         <div class="g_9">
-                            <input class="simple_field" type="text" name="zuozhe" required/>
+                            <input class="simple_field" type="text" name="zuozhe" value="{{$list->author}}" disabled/>
                         </div>
                     </div>
                     <div class="line_grid">
                         <div class="g_3"><span class="label">文章来源<span class="must">*</span></span></div>
                         <div class="g_9">
-                            <input class="simple_field" type="text" name="laiyuan" required/>
+                            <input class="simple_field" type="text" name="laiyuan" value="{{$list->source}}" disabled/>
                         </div>
                     </div>
                     <div class="line_grid">
