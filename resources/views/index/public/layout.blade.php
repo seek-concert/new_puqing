b97f321c56302c21d93ec0f03d0e171b2d1c6c03
seek-concert <1105420247@qq.com>
2019-07-07 14:17:00 +0800

    普擎科技-自动推送

--- resources/views/index/public/layout.blade.php
+++ resources/views/index/public/layout.blade.php
@@ -14,6 +14,26 @@
     <link rel="canonical" href="/">
     <link rel="stylesheet" href="/index/css/bootstrap-reboot.min.css">
     <link rel="stylesheet" href="/index/css/bootstrap.min.css">
+    <script>
+        /*百度*/
+        (function(){
+            var bp = document.createElement('script');
+            var curProtocol = window.location.protocol.split(':')[0];
+            if (curProtocol === 'https') {
+                bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+            }
+            else {
+                bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+            }
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(bp, s);
+        })();
+        /*360*/
+        (function(){
+            var src = (document.location.protocol == "http:") ? "http://js.passport.qihucdn.com/11.0.1.js?61efbc491fee6c068e59f95e654885e7":"https://jspassport.ssl.qhimg.com/11.0.1.js?61efbc491fee6c068e59f95e654885e7";
+            document.write('<script src="' + src + '" id="sozz"><\/script>');
+        })();
+    </script>
     @yield('css')
 </head>
 
